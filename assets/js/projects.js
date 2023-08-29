b748f83a0b906ac612157293dca8f7382423c982
Cid <32217136+cidxb@users.noreply.github.com>
2023-08-29 13:55:50 +0800

    Update projects.js

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -11,7 +11,7 @@ let render_projects = (slug) => {
 
     let projects_obj = [
         {
-            image: 'assets/images/mentors.jpg',
+            image: 'assets/images/RobotJuggle.jpg',
             link: '',
             title: 'Robotic Arm Juggling PingPong Ball',
             demo: 'https://mporter.co',
